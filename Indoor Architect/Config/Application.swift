7b91c1cedd38f7afba234e690596d53fffb63017
Dennis Prudlo <mail@dennisprudlo.com>
2020-05-05 15:47:13 +0200

    Add the reference type

--- Indoor Architect/Config/Application.swift
+++ Indoor Architect/Config/Application.swift
@@ -31,6 +31,20 @@ struct Application {
 	/// A reference to the root view controller which is set after its initialization
 	static var rootController: RootController!
 	
+	/// A reference to the currently selected project
+	///
+	/// In most cases the `IMDFProject` is used and more rather needed in every view to get at least
+	/// a reference to the IMDF Features. One approach would be to pass the reference of the project down
+	/// to every controller which ends with every controller having a project reference. When a project is selected
+	/// The reference is being assigned to this property and can be used throughout the app.
+	///
+	/// The project reference may be nil, when no project is selected. This could be when a guide or resource is visible
+	/// - Important:
+	///	It is very important to make sure that this property always represents the current project selection state.
+	/// Therefore the property is an implicitly unwrapped `IMDFProject`. We assume at any given time in a project
+	/// context, that this property is set.
+	static var currentProject: IMDFProject!
+	
 	/// Gets a RFC 5646 compliant language tag for the current locale
 	///
 	/// If for some reason the language tag couldn't be obtained the property falls back to `en-US`
